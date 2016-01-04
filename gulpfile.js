c287324000fd967805c208a8e70fb7cd8e69e25c
wellhairy <AllanKing27@gmail.com>
2016-01-04 13:58:09 +0000

    create working css model

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
